0ba620e2f0e27a28e1d26699223d35ec322f457a
Deepam Wagh <deepam.wagh@gmail.com>
2025-12-01 14:21:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-//your JS code here. If required.
-const sounds = ["sound1.mp3", "sound2.mp3", "sound3.mp3", "sound4.mp3"];
+const sounds = [
+  "audio1.mp3",
+  "audio2.mp3",
+  "audio3.mp3",
+  "audio4.mp3",
+  "audio5.mp3",
+  "audio6.mp3"
+];
+
 const buttonsContainer = document.getElementById("buttons");
 let currentAudio = null;
 
-
 sounds.forEach(sound => {
-const btn = document.createElement("button");
-btn.className = "btn";
-btn.innerText = sound.replace(".mp3", "");
+  const btn = document.createElement("button");
+  btn.className = "btn";
+  btn.innerText = sound.replace(".mp3", "");
 
-
-btn.addEventListener("click", () => {
-if (currentAudio) currentAudio.pause();
-currentAudio = new Audio(`sounds/${sound}`);
-currentAudio.play();
-});
-buttonsContainer.appendChild(btn);
+  btn.addEventListener("click", () => {
+    if (currentAudio) currentAudio.pause();
+    currentAudio = new Audio(`sounds/${sound}`);
+    currentAudio.play().catch(() => {});
+  });
+  buttonsContainer.appendChild(btn);
 });
 
 const stopBtn = document.createElement("button");
 stopBtn.className = "stop";
 stopBtn.innerText = "STOP";
-
 stopBtn.addEventListener("click", () => {
-if (currentAudio) {
-currentAudio.pause();
-currentAudio.currentTime = 0;
-}
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
 });
+
 buttonsContainer.appendChild(stopBtn);
